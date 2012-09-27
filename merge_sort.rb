778ddd4a157eff577cb9f00844da8bb2fa0a4241
Nikhil Lingutla <nikhil.lingutla@gmail.com>
2012-09-27 14:13:41 -0700

    Added MergeSort

---
+++ merge_sort.rb
@@ -0,0 +1,41 @@
+def merge(left, right)
+  length = left.length + right.length
+  sorted = []
+  while sorted.length != length do
+    if left.length == 0
+      sorted = (sorted << right).flatten
+    elsif right.length == 0
+      sorted = (sorted << left).flatten
+    elsif left[0] > right[0]
+      sorted << right[0]
+      right.shift
+    else
+      sorted << left[0]
+      left.shift
+    end
+  end
+  sorted
+end
+
+def merge_sort(numbers)
+  length = numbers.length
+  if length == 1
+    sorted = numbers
+  else
+    left   = merge_sort(numbers[0..(length-1)/2])
+    right  = merge_sort(numbers[((length-1)/2+1)..-1])
+    sorted = merge(left, right)
+  end
+  sorted
+end
+
+numbers = []
+ARGV.each do|a|
+  numbers << Integer(a)
+end
+
+sorted = merge_sort(numbers)
+puts "sorted sequence using merge sort is "
+sorted.each do |num|
+  print "#{num} "
+end
