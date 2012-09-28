05a4a2d150a4fa41c49e727a1e2aa4299625aa88
Nikhil Lingutla <nikhil.lingutla@gmail.com>
2012-09-27 18:31:37 -0700

    Improved binary search

--- binary_search.rb
+++ binary_search.rb
@@ -1,23 +1,16 @@
 def binary_search(numbers, key)
   length = numbers.length
-  if length == 1
-    if numbers[0] == key
-      puts "key found at position 1"
-    else
-      puts "Key not found"
-      Process.exit
-    end
-  end
   mid = length/2
   if numbers[mid] == key
     puts "number found at position #{mid+1}"
+    Process.exit
+  elsif length == 1
+    puts "Key #{key} not found"
+    Process.exit
   elsif  numbers[mid] > key
     binary_search(numbers[0..mid], key)
   elsif numbers[mid] < key
     binary_search(numbers[(mid+1)..-1], key)
-  else
-    puts "Key #{key} not found"
-    Process.exit
   end
 end
 
